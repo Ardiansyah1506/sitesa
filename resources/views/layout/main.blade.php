89024609e8a3e41b596c3d434762a458e9dd03fe
Dyaka Faiz <dyakafaiz123@gmail.com>
2024-07-26 22:57:51 +0700

    sidebar layout fix

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,38 +4,7 @@
   <body>
     <div class="wrapper">
       <!-- Sidebar -->
-      <div class="sidebar" data-background-color="dark">
-        <div class="sidebar-logo">
-          <!-- Logo Header -->
-          <div class="logo-header" data-background-color="dark">
-            <a href="#" class="logo">
-              <img
-                src="assets/img/kaiadmin/logo_light.svg"
-                alt="navbar brand"
-                class="navbar-brand"
-                height="20"
-              />
-            </a>
-            <div class="nav-toggle">
-              <button class="btn btn-toggle toggle-sidebar">
-                <i class="gg-menu-right"></i>
-              </button>
-              <button class="btn btn-toggle sidenav-toggler">
-                <i class="gg-menu-left"></i>
-              </button>
-            </div>
-            <button class="topbar-toggler more">
-              <i class="gg-more-vertical-alt"></i>
-            </button>
-          </div>
-          <!-- End Logo Header -->
-        </div>
-        <div class="sidebar-wrapper scrollbar scrollbar-inner">
-          <div class="sidebar-content">
-            @include('layout.sidebar')
-          </div>
-        </div>
-      </div>
+      @include('layout.sidebar')
       <!-- End Sidebar -->
 
       <div class="main-panel">
---
+++ resources/views/layout/sidebar/sidebar-prodi.blade.php
@@ -0,0 +1,6 @@
+<li class="nav-item active">
+    <a href="../../documentation/index.html">
+      <i class="fas fa-home"></i>
+      <p>Dashboard</p>
+    </a>
+  </li>
