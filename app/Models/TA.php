cb8eeffc6c2be3fbffd91c54629f8c33577d5833
Dyaka Faiz <dyakafaiz123@gmail.com>
2024-07-29 18:37:17 +0700

    cek

--- app/Models/TA.php
+++ app/Models/TA.php
@@ -5,11 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TA extends Model
+class Ta extends Model
 {
     use HasFactory;
-    protected $table = "ta";
-    protected $fillable = [
-        'id', 'kode_ta', 'nim', 'nama_file', 'tanggal', 'status', 'nota_pembimbing'
-    ];
 }
